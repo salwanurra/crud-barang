5c8616e34aaf3f5c5bc31fd7e23902ab63ae688e
Salwa Nur A <79437142+salwanurra@users.noreply.github.com>
2021-09-02 13:25:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<html>
+	<head>
+		<title>LSP BPPTIK</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+ 		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-light bg-light" aria-label="Main navigation">
+		  <div class="container-fluid">
+		    <a class="navbar-brand">
+		    	<img src="images/bpptik.png" alt="" height="50">
+		    </a>
+		    <form class="d-flex">
+		      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+		      <button id="search" class="btn btn-warning" type="submit">Search</button>
+		    </form>
+  		  </div>
+		</nav>
+		<nav class="nav" aria-label="Secondary navigation">
+			<ul class="nav">
+				<li class="nav-item">
+					<a class="nav-link" href="index.php" style="margin-left: 5%; color:yellow;">Home</a>
+				</li>
+				<li class="nav-item dropdown" id="dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">LSP</a>
+					<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<li><a href="visi.php" class="dropdown-item">Visi dan Misi LSP BPPTIK</a></li>
+						<li><a href="organisasi.php" class="dropdown-item">Struktur Organisasi LSP BPPTIK</a></li>
+						<li><a href="personil.php" class="dropdown-item">Personil LSP BPPTIK</a></li>
+						<li><a href="lingkup.php" class="dropdown-item">Ruang Lingkup LSP BPPTIK</a></li>
+						<li><a href="skema.php" class="dropdown-item">Skema Sertifikasi</a></li>
+					</ul>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="pelatihan.php">Pelatihan</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="sertifikasi.php">Sertifikasi</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="jadwal.php">Jadwal</a>
+				</li>
+			</ul>
+		</nav>
+
+	<div class="container" style="margin-top: 1%;">
+		<div class="row g-5">
+			<div class="col-md-9 bg-white rounded">
+				<h4 style="color: #0E4C92; margin-top: 3%;">LSP BPPTIK</h4><hr>
+				<div class="row">
+					<div class="col-md-6">
+						<img src="images/lsp.png" alt="">
+					</div>
+					<div class="col-md-6">
+						<img src="images/bnsp.jpeg" alt="">
+					</div>
+				</div>
+				<br />
+				<p>LSP BPPTIK adalah lembaga pendukung BNSP yang  bertanggung jawab untuk melaksanakan uji kompetensi dan sertifikasi kompetensi di bidang TIK. LSP BPPTIK didirikan oleh Badan Penelitian dan Pengembangan Sumber Daya Manusia (Balitbang SDM) Kementerian Komunikasi dan Informatika (Kominfo) melalui Surat Keputusan (SK) Kepala Balitbang SDM Nomor 13 Tahun 2018 tanggal 1 Februari 2018 tentang Pembentukan Lembaga Sertifikasi Profesi Pihak Kesatu Balai Pelatihan dan Pengembangan Teknologi Informasi dan Komunikasi. <br />
+				LSP BPPTIK memiliki fungsi melaksanakan sertifikasi kompetensi uji kompetensi, dan sertifikasi bidang TIK, dengan tugas:
+				</p>
+				<ul style="list-style-type:lower-alpha;">
+					<li>menyusun dan mengembangkan skema sertifikasi</li>
+					<li>membuat perangkat asesmen dan uji kompetensi</li>
+					<li>menyediakan tenaga penguji (asesor)</li>
+					<li>melaksanakan uji kompetensi dan sertifikasi</li>
+					<li>melaksanakan surveilan pemeliharaan sertifikasi</li>
+					<li>menetapkan persyaratan, memverifikasi dan menetapkan Tempat Uji Kompetensi (TUK)</li>
+					<li>memelihara kinerja asesor dan TUK dan</li>
+					<li>mengembangkan pelayanan uji kompetensi dan sertifikasi.</li>
+				</ul>
+				<p>Dalam melaksanakan fungsi dan tugasnya, LSP BPPTIK mengacu pada pedoman yang dikeluarkan oleh BNSP. Dalam pedoman tersebut ditetapkan persyaratan yang harus dipatuhi untuk menjamin agar lembaga sertifikasi menjalankan sistem sertifikasi secara konsisten dan profesional, sehingga dapat diterima pada tingkat nasional yang relevan demi kepentingan pengembangan sumber daya manusia dalam aspek peningkatan kualitas dan perlindungan tenaga kerja.</p>
+				<ul style="list-style-type:square;">
+					<li><a href="visi.php">Visi Dan Misi LSP BPPTIK</a></li>
+					<li><a href="organisasi.php">Struktur Organisasi LSP BPPTIK</a></li>
+					<li><a href="personil.php">Personil LSP BPPTIK</a></li>
+					<li><a href="lingkup.php">Ruang Lingkup LSP BPPTIK</a></li>
+					<li><a href="skema.php">Skema Sertifikasi</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<div class="p-4 mb-3 bg-white rounded">
+					<a href="pelatihan.php"><img src="images/tik.jpeg" alt="" style="width: 100%"></a>
+				</div>
+				<div class="p-4 bg-white rounded">
+					<a href="pelatihan.php"><img src="images/skkni.jpeg" alt="" style="width: 100%"></a>
+				</div>
+				<br>
+					<div class="card" style="width: 16rem; background-color: #0E4C92; color: white;">
+					  <div class="card-body">
+					    <h6 class="card-title">News Letter</h6>
+					    <p class="card-text" style="text-align: center; font-size: 15px;">Daftarkan e-mail Anda ke Newsletter BPPTIK untuk mendapatkan update dari kami: <br>
+					    	<input type="text" style="margin-bottom:3%;"> 
+					    	<button type="button" class="btn btn-light btn-sm">Daftar</button>
+					    	<br>
+						Setelah mendaftar Newsletter, jangan lupa segera buka e-mail Anda untuk mengaktivasikan Newsletternya. (Catatan: jika e-mail dari kami tidak ada di inbox e-mail Anda, mohon periksa pada folder bulk/spam e-mail Anda)</p>
+					  </div>
+					</div>
+					<br>
+					<div class="card" style="width: 16rem; background-color: #0E4C92; color: white;">
+					  <div class="card-body">
+					    <h6 class="card-title">Link Terkait</h6>
+					    <div class="text-center">
+					    <a href="https://lan.go.id/"><img src="images/lembaga.png" alt="" width="50"></a>
+					    <a href="https://kominfo.go.id/"><img src="images/kominfo.png" alt="" width="50"></a>
+					    <a href="https://bnsp.go.id/"><img src="images/bnsp2.png" alt="" width="50"></a>
+						</div>
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br />
+		<footer class="footer">
+				<p><strong>Info Lainnya</strong></p>
+				<p><a href="#">LOKASI HIBURAN</a></p>
+				<p><a href="#" id="stat">STATISTIK KOMINFO</a></p>
+		</footer>
+		<div class="align-bottom" id="stiky">
+				<p>Copyright &copy 2021 Salwa Nur</p>
+		</div>	
+	</body>
+</html>
